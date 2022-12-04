5ccf9ab34b5f128c0c71e7dceab8e540bd60a133
Erwin Wessels <erwin@pleo.io>
2022-12-04 09:35:46 +0100

    Day 04

--- src/Day04.kt
+++ src/Day04.kt
@@ -2,17 +2,40 @@
 
 private const val DAY = "04"
 
-private const val PART1_CHECK = 0
-private const val PART2_CHECK = 0
+private const val PART1_CHECK = 2
+private const val PART2_CHECK = 4
+
+private operator fun IntRange.contains(other: IntRange): Boolean {
+    return (first >= other.first && last <= other.last) ||
+        (other.first >= first && other.last <= last)
+}
+
+private infix fun IntRange.overlap(other: IntRange): Boolean {
+    return other.first in first..last || other.last in first..last ||
+            first in other.first..other.last || last in other.first .. other.last
+}
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+        input.map { line ->
+            line.split(',').map { elf ->
+                val (start, stop) = elf.split('-').map { it.toInt() }
+                IntRange(start, stop)
+            }
+        }.count { (elfA, elfB) ->
+            elfA in elfB
+        }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int =
+        input.map { line ->
+            line.split(',').map { elf ->
+                val (start, stop) = elf.split('-').map { it.toInt() }
+                IntRange(start, stop)
+            }
+        }.count { (elfA, elfB) ->
+            elfA overlap elfB
+        }
+
 
     println("Day $DAY")
     // test if implementation meets criteria from the description, like:
